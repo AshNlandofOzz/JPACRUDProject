a975dec7070a3483fbf32dadeb67243e653eff41
AshNlandofOzz <ashleyosburn88@gmail.com>
2022-10-14 16:29:00 -0600

    Create Boot/MVC Project

---
+++ FishTankWaterTest/src/main/java/com/skilldistillery/jpafishtankwatertest/FishTankWaterTestApplication.java
@@ -0,0 +1,13 @@
+package com.skilldistillery.jpafishtankwatertest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FishTankWaterTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FishTankWaterTestApplication.class, args);
+	}
+
+}
